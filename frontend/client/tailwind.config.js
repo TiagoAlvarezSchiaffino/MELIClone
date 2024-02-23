1a543c15e52d7f515e80d696d4bccebec9fb3b43
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-23 18:11:34 -0300

    feat: Integrate color palette and font-family

--- frontend/client/tailwind.config.js
+++ frontend/client/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*/.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#FEE602',
+        secondary: '#FFF159',
+        red: '#E40002',
+        darkblue: '#2D3277',
+        ligthblue: '#3483FA',
+        green: '#00A650',
+        grey: '#EDEDED'
+      }
+    }
   },
   plugins: []
 };
