eb7847e4820a770a02b9c81b948270c66fea6880
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-02-23 23:03:27 -0300

    refactor: Update HeroCategory component to display images

--- frontend/client/src/components/Hero/HeroCategory/HeroCategory.jsx
+++ frontend/client/src/components/Hero/HeroCategory/HeroCategory.jsx
@@ -1,13 +1,27 @@
-import HeroBenefit from './HeroBenefit/HeroBenefit'
-import HeroCategory from './HeroCategory/HeroCategory'
-
-const Hero = () => {
+import electrodomestic from '../../../assets/img/electrodomestic.png';
+import phone from '../../../assets/img/phone.png';
+import shoes from '../../../assets/img/shoes.png';
+import house from '../../../assets/img/house.png';
+const HeroCategory = () => {
   return (
-    <>
-      <HeroBenefit />
-      <HeroCategory />
-    </>
+    <div className='bg-black'>
+      <h2 className=' text-center text-3xl font-bold'> EXPLORÁ POR CATEGORÍA </h2>
+      <div className='flex flex-row'>
+        <article className=''>
+          <img src={electrodomestic} alt='electrodomestic' />
+        </article>
+        <article className=''>
+          <img src={phone} alt='phone' />
+        </article>
+        <article className=''>
+          <img src={shoes} alt='shoes' />
+        </article>
+        <article className=''>
+          <img src={house} alt='house' />
+        </article>
+      </div>
+    </div>
   )
 }
 
-export default Hero
+export default HeroCategory
