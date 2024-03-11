9ba46021a91c813e2fc06d2fb6938e6cc9b659db
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-10 23:17:23 -0300

    feat: HeroBenefit and HeroCategory implementation

--- frontend/client/src/components/Hero/HeroBenefit/HeroBenefit.jsx
+++ frontend/client/src/components/Hero/HeroBenefit/HeroBenefit.jsx
@@ -1,7 +1,67 @@
+import boom from "../../../assets/icons/boom.svg";
+import coupon from "../../../assets/icons/coupon.svg";
+import credict_card from "../../../assets/icons/credict_card.svg";
+import fire from "../../../assets/icons/fire.svg";
+import full from "../../../assets/icons/full.svg";
+import porcents from "../../../assets/icons/porcents.svg";
+import shoes from "../../../assets/icons/shoes.svg";
+import thunder from "../../../assets/icons/thunder.svg";
 const HeroBenefit = () => {
   return (
-    <div>
-      <h2>DISFRUTÁ DE LOS MEJORES BENEFICIOS</h2>
+    <div className="h-1/4 ">
+      <h2 className=" text-center text-base font-medium pt-6 tracking-[1.6px]">
+        DISFRUTÁ DE LOS MEJORES BENEFICIOS
+      </h2>
+      <div className="flex flex-wrap  justify-center mt-4 gap-10">
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={fire} alt="fire" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡OFERTAS SÓLO POR HOY!</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={thunder} alt="thunder" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">OFERTAS RELÁMPAGO</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={porcents} alt="porcents" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡LOS MAS VENDIDOS!</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={boom} alt="boom" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡MEGA BOMBAS DEL HOT SALE!</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={credict_card} alt="credict_card" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡COMPRÁ EN 6 CUOTAS FIJAS!</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={coupon} alt="coupon" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡CUPONES CON DESCUENTO!</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={shoes} alt="shoes" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡SELECCIONADOS DE ADIDAS!</p>
+        </article>
+        <article className="mb-8 flex flex-col justify-center items-center">
+          <div className="bg-white rounded-full border-[10px] border-white">
+            <img className="rounded-full" src={full} alt="full" />
+          </div>
+          <p className="mt-4 text-center w-[96px] text-xs">¡LOS ENVÍOS MÁS RÁPIDOS!</p>
+        </article>
+      </div>
     </div>
   )
 }
--- frontend/client/src/components/Hero/HeroCategory/HeroCategory.jsx
+++ frontend/client/src/components/Hero/HeroCategory/HeroCategory.jsx
@@ -1,23 +1,35 @@
-import electrodomestic from '../../../assets/img/electrodomestic.png';
-import phone from '../../../assets/img/phone.png';
-import shoes from '../../../assets/img/shoes.png';
-import house from '../../../assets/img/house.png';
+import electrodomestic from "../../../assets/img/electrodomestic.png";
+import phone from "../../../assets/img/phone.png";
+import shoes from "../../../assets/img/shoes.png";
+import house from "../../../assets/img/house.png";
+import style from "./heroCategory.module.css";
 const HeroCategory = () => {
   return (
-    <div className='bg-black'>
-      <h2 className=' text-center text-3xl font-bold'> EXPLORÁ POR CATEGORÍA </h2>
-      <div className='flex flex-row'>
-        <article className=''>
-          <img src={electrodomestic} alt='electrodomestic' />
+    <div>
+      <h2 className=" text-center text-base font-medium pt-6 tracking-[1.6px]">
+        {" "}
+        EXPLORÁ POR CATEGORÍA{" "}
+      </h2>
+      <div className=" flex flex-col sm:flex-row items-center justify-center gap-9 my-10 gro pb-6">
+        <article
+          className={`${style.article} w-[284px] sm:w-[284px] shadow-lg shadow-slate-600 rounded-lg`}
+        >
+          <img className="w-full" src={electrodomestic} alt="electrodomestic" />
         </article>
-        <article className=''>
-          <img src={phone} alt='phone' />
+        <article
+          className={`${style.article} w-[284px] sm:w-[284px] shadow-lg shadow-slate-600 rounded-lg`}
+        >
+          <img className="w-full" src={phone} alt="phone" />
         </article>
-        <article className=''>
-          <img src={shoes} alt='shoes' />
+        <article
+          className={`${style.article} w-[284px] sm:w-[284px] shadow-lg shadow-slate-600 rounded-lg`}
+        >
+          <img className="w-full" src={shoes} alt="shoes" />
         </article>
-        <article className=''>
-          <img src={house} alt='house' />
+        <article
+          className={`${style.article} w-[284px]  sm:w-[284px] shadow-lg shadow-slate-600 rounded-lg`}
+        >
+          <img className="w-full" src={house} alt="house" />
         </article>
       </div>
     </div>
