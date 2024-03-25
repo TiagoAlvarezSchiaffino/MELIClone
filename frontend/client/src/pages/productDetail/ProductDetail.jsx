809ed43fb9866a1bb4566606861ddb46fcf48c83
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-25 18:03:48 -0300

    feat: Add banner without image

--- frontend/client/src/pages/productDetail/ProductDetail.jsx
+++ frontend/client/src/pages/productDetail/ProductDetail.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import Carousel from './components/Carousel';
+import DescriptionAside from './components/DescriptionAside';
+import DescriptionBottom from './components/DescriptionBottom';
+import QuestionSection from './components/QuestionSection';
+import BuySection from './BuySection';
+import StoreDetail from './StoreDetail';
+import { useParams } from 'react-router-dom';
+import { Product } from './components/Product';
+import banner from '../../assets/img/banner.svg';
 
 const ProductDetail = () => {
+  const { id } = useParams();
+
   return (
-    <div>ProductDetail</div>
-  )
-}
+    <div>
+      <div className='bg-grey flex flex-col justify-center content-center px-24 py-14 w-100'>
+        <div className='flex justify-between w-100 my-2'>
+          <div>
+            <button>Volver</button>
+            <span className='mx-1'>|</span>
+            <button className='text-ligthblue'>Celulares y teléfonos</button>
+            <span className='mx-1 text-ligthblue'>-</span>
+            <button className='text-ligthblue'>Celulares y smartphones</button>
+          </div>
+          <div>
+            <button className='text-ligthblue'>Compartir</button>
+            <span className='text-ligthblue mx-1'>|</span>
+            <button className='text-ligthblue'>Vender uno igual</button>
+          </div>
+        </div>
+        <div className='flex'>
+          <div className='bg-white flex flex-col w-100 py-14'>
+            <div className='flex w-100 mx-16'>
+              <div className='bg-white w-3/6'>
+                <Carousel images={Product.images} />
+              </div>
+              <div className='bg-white w-3/6'>
+                <DescriptionAside
+                  name={Product.name}
+                  characteristics={Product.characteristics}
+                  specification={Product.specification}
+                  price={Product.price}
+                  off={Product.off}
+                />
+              </div>
+            </div>
+            <hr className='flex mx-16 my-8' />
+            <div className='w-100 mx-16'>
+              <DescriptionBottom description={Product.description} />
+            </div>
+            <hr className='flex mx-16 my-8' />
+            <div className='mx-16'>
+              <QuestionSection />
+            </div>
+          </div>
+          <div className='bg-white w-4/12 flex flex-col pt-6 pr-6 gap-4'>
+            <BuySection />
+            <StoreDetail />
+          </div>
+        </div>
+        <div>Sugerencias</div>
+        <div className='flex justify-center w-100'>
+          <img src={banner} alt='banner' className='w-100' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetail
--- frontend/client/src/pages/productDetail/components/DescriptionAside.jsx
+++ frontend/client/src/pages/productDetail/components/DescriptionAside.jsx
@@ -1,36 +1,78 @@
-import {AiOutlineHeart} from "react-icons/ai"
-const DescriptionAside =({name,characteristics,price,off})=>{
-    return(
-        <div className="flex flex-col p-2">
-            <div className="flex justify-between p-1 text-gray-500 text-sm">
-                <p>Nuevo | +1000 vendidos</p>
-                <p className="text-3xl text-ligthblue cursor-pointer"><AiOutlineHeart/></p>
-            </div>
-            <div className="my-2 p-1">
-                <h2 className="font-bold text-2xl">{name}</h2>
-            </div>
-            <div className="my-2 p-1">
-                <p className="line-through text-gray-500">${price}</p>
-                <div className="flex items-center ">
-                    <h3 className="text-4xl font-light">${price-off}</h3>
-                    <p className="text-green mx-2 text-lg">{off}% OFF</p>
-                </div>
-            </div>
-            <div>
-                <p className="text-ligthblue my-2"><a href="">Ver los medios de pago</a></p>
-                <p className="bg-ligthblue text-white text-sm inline p-1 rounded"><a href="">OFERTA DEL DIA</a></p>
-            </div>
-            <div className="my-2 p-1">
-                <ul>
-                    {characteristics.map((elem)=>{
-                        return(
-                            <li className="list-disc py-1 text-gray-500 text-sm">{elem}</li>
-                        )
-                    })}
-                </ul>
-            </div>
+import { AiOutlineHeart } from "react-icons/ai";
+import { useState } from "react";
+const DescriptionAside = ({ name, characteristics, specification, price, off }) => {
+  const [color, setColor] = useState(specification.color[0]);
+  const [colorSelected, setColorSelected] = useState(color);
+  const active = "border-ligthblue border-2";
+  return (
+    <div className="flex flex-col p-2">
+      <div className="flex justify-between p-1 text-gray-500 text-sm">
+        <p>Nuevo | +1000 vendidos</p>
+        <p className="text-3xl text-ligthblue cursor-pointer">
+          <AiOutlineHeart />
+        </p>
+      </div>
+      <div className="my-2 p-1">
+        <h2 className="font-bold text-2xl">{name}</h2>
+      </div>
+      <div className="my-2 p-1">
+        <p className="line-through text-gray-500">${price}</p>
+        <div className="flex items-center ">
+          <h3 className="text-4xl font-light">${price - off}</h3>
+          <p className="text-green mx-2 text-lg">{off}% OFF</p>
         </div>
-    )
-}
-
+      </div>
+      <div>
+        <p className="text-ligthblue my-2">
+          <a href="">Ver los medios de pago</a>
+        </p>
+        <p className="bg-ligthblue text-white text-sm inline p-1 rounded">
+          <a href="">OFERTA DEL DIA</a>
+        </p>
+      </div>
+      <div className="py-3">
+        <p className="text-black text-lg">
+          Color: <span>{color}</span>
+        </p>
+        <div className="flex py-2 flex-wrap">
+          {specification.color.map(elem => {
+            return (
+              <button
+                key={specification.color.indexOf(elem)}
+                onClick={() => {
+                  setColorSelected(elem);
+                }}
+                onMouseEnter={() => {
+                  setColor(elem);
+                }}
+                onMouseOut={() => {
+                  setColor(colorSelected);
+                }}
+                className={`hover:border-ligthblue ${
+                  elem == colorSelected && active
+                } mx-2 my-1 py-2 px-3 rounded-lg border hover:border-ligthblue`}
+              >
+                {elem}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="my-2 p-1">
+        <ul>
+          {characteristics.map(elem => {
+            return (
+              <li
+                key={characteristics.indexOf(elem)}
+                className="list-disc py-1 text-gray-500 text-sm"
+              >
+                {elem}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default DescriptionAside
