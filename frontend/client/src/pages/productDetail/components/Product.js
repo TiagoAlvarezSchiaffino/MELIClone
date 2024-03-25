fedbd2aca7f354a8c5d966356c2a289235d06d68
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-25 18:02:58 -0300

    feat: Add QuestionSection

---
+++ frontend/client/src/pages/productDetail/components/Product.js
@@ -0,0 +1,40 @@
+export const Product = {
+    name: "Samsung Galaxy A54 5g 256gb 8gb Ram Awesome Graphite",
+    images: [
+      "https://http2.mlstatic.com/D_NQ_NP_987793-MLA54614253497_032023-O.webp",
+      "https://http2.mlstatic.com/D_NQ_NP_938631-MLA54614297095_032023-O.webp",
+      "https://http2.mlstatic.com/D_NQ_NP_673617-MLA54614434255_032023-O.webp",
+      "https://http2.mlstatic.com/D_NQ_NP_704347-MLA54617291492_032023-O.webp",
+      "https://http2.mlstatic.com/D_NQ_NP_617389-MLA54617424542_032023-O.webp",
+      "https://http2.mlstatic.com/D_NQ_NP_683383-MLA54617606314_032023-O.webp",
+      "https://http2.mlstatic.com/D_NQ_NP_716903-MLA54617361036_032023-O.webp"
+    ],
+    characteristics: [
+      "Dispositivo liberado para que elijas la compañía telefónica que prefieras.",
+      "Compatible con redes 5G.",
+      "Pantalla de 6.4'",
+      "Cámara delantera de 32Mpx.",
+      "Batería de 5000mAh.",
+      "Memoria interna de 256GB."
+    ],
+    specification: {
+      marca: "Samsung",
+      modelo: "Galaxy A54",
+      color: ["Negro", "Gris Oscuro", "Blanco", "Rojo", "Verde", "Rosa", "Amarillo"],
+      sistemaOperativo: "Android",
+      pantallaSize: "6.4'",
+      pantallaTipoResolucion: "Full HD+",
+      pantallaResolucion: "1080px x 2340px",
+      bateria: "5000 mAh",
+      dualSim: true
+    },
+    description: [
+      "Capacidad y eficiencia: Con su potente procesador y memoria RAM de 8 GB tu equipo alcanzará un alto rendimiento con gran velocidad de transmisión de contenidos y ejecutará múltiples aplicaciones a la vez sin demoras.",
+      "Batería de duración superior: ¡Desenchufate! Con la súper batería de 5000 mAh tendrás energía por mucho más tiempo para jugar, ver series o trabajar sin necesidad de realizar recargas. ",
+      "Capacidad de almacenamiento ilimitada: Olvidate de borrar Con su memoria interna de 256 GB podrás descargar todos los archivos y aplicaciones que necesites, guardar fotos y almacenar tus películas, series y videos favoritos para reproducirlos cuando quieras."
+    ],
+    price: 184.999,
+    off: 13,
+    seller: "Agustin y Francisco",
+    quantity: 2
+  };
---
+++ frontend/client/src/pages/productDetail/components/QuestionSection.jsx
@@ -0,0 +1,88 @@
+import { AiOutlineSearch, AiOutlineQuestionCircle } from "react-icons/ai";
+import { RxCornerBottomLeft } from "react-icons/rx";
+import { useState } from "react";
+const QuestionSection = () => {
+  const question = [
+    {
+      pregunta: "¿cómo te llamas?",
+      respuesta: "Miguel"
+    },
+    {
+      pregunta: "¿De dónde eres?",
+      respuesta: "Argentina"
+    },
+    {
+      pregunta: "¿Qué comiste?",
+      respuesta: "Queti"
+    }
+  ];
+  const [moreQ, setMoreQ] = useState(false);
+  const changeQ = () => {
+    moreQ ? setMoreQ(false) : setMoreQ(true);
+  };
+  return (
+    <div className="flex flex-col gap-6">
+      <h3 className="text-3xl">Preguntas y respuestas</h3>
+      <h4 className="text-xl font-bold py-2">¿Qué querés saber?</h4>
+      <div className="w-100 py-5">
+        <button className="bg-blue-100 text-ligthblue text-xl p-2 mx-1 rounded-md">
+          Costo y tiempo de envío
+        </button>
+        <button className="bg-blue-100 text-ligthblue text-xl p-2 mx-1 rounded-md">
+          Devoluciones gratis
+        </button>
+        <button className="bg-blue-100 text-ligthblue text-xl p-2 mx-1 rounded-md">
+          Medios de pago y promociones
+        </button>
+        <button className="bg-blue-100 text-ligthblue text-xl p-2 mx-1 rounded-md">Garantía</button>
+      </div>
+      <h4 className="text-xl font-bold py-2">Buscá lo que querés saber</h4>
+      <div className="inline-flex w-9/12 my-2 border border-gray-400 rounded">
+        <input
+          type="text"
+          placeholder="Escribí una pregunta o palabra clave..."
+          className="w-11/12 p-3 text-gray-500 rounded"
+        />
+        <div className="bg-ligthblue p-3 w-1/12 flex justify-center content-center items-center text-3xl cursor-pointer">
+          <AiOutlineSearch className="text-white" />
+        </div>
+      </div>
+      <h4 className="text-xl font-bold py-2 my-2">Últimas realizadas</h4>
+      <div className="">
+        <div key={0} className="p-2">
+          <p className="text-black text-lg">{question[0].pregunta}</p>
+          <p className="text-gray-500 mx-8 text-lg flex">
+            <RxCornerBottomLeft /> {question[0].respuesta}
+          </p>
+        </div>
+        {moreQ &&
+          question.map(elem => {
+            if (question.indexOf(elem) !== 0) {
+              return (
+                <div key={question.indexOf(elem)} className="p-2">
+                  <p className="text-black text-lg">{elem.pregunta}</p>
+                  <p className="text-gray-500 mx-8 text-lg flex">
+                    <RxCornerBottomLeft />
+                    {elem.respuesta}
+                  </p>
+                </div>
+              );
+            }
+          })}
+        <button
+          onClick={() => {
+            changeQ();
+          }}
+          className="text-ligthblue text-xl my-3"
+        >
+          <p>Ver {!moreQ ? question.length - 1 + " preguntas más" : "Menos"}</p>
+        </button>
+      </div>
+      <div className="bg-blue-100 text-ligthblue text-2xl w-5/12 my-3 flex justify-center items-center rounded-md">
+        <AiOutlineQuestionCircle />
+        <button className="py-3 mx-1">¿Cómo pregunto al vendedor?</button>
+      </div>
+    </div>
+  );
+};
+export default QuestionSection
