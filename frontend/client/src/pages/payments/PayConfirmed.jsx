8c9217c77de295a4ed818a154d08485207914a26
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-26 14:01:03 -0300

    feat: Add Responsive to PayConfirmed 'pages'

--- frontend/client/src/pages/payments/PayConfirmed.jsx
+++ frontend/client/src/pages/payments/PayConfirmed.jsx
@@ -9,43 +9,59 @@ import location from '../../assets/icons/location.png';
 const PayConfirmed = () => {
   return (
     <>
-      <NavbarPayment />
+      <NavbarPayment title='Revisá y confirmá tu compra' />
 
-      <div className=' bg-[#EEEEEE] flex justify-around'>
-        <div className='my-10'>
-          <h2 className='text-[24px] font-[500] text-[#333333] mb-5'>Revisá y confirmá tu compra</h2>
+      <div className="bg-[#EEEEEE] flex flex-col lg:flex-row justify-center items-center lg:items-stretch lg:justify-around">
+        <div className="my-10 mx-6 sm:ml-14 sm:mr-0">
+          <h2 className="hidden sm:block text-[24px] font-[500] text-[#333333] mb-5">
+            Revisá y confirmá tu compra
+          </h2>
           <p className='text-lg text-[#333333] font-medium mb-3'>Detalle de la entrega</p>
-          <CardItem>
+          <CardItem bg="-[#F5F5F5]">
             <div>
-              <img src={location} alt='Icono de tienda' className='border rounded-full border-none bg-black w-[18px] h-[18px]' />
+              <div className="border rounded-full border-none bg-white w-[48px] h-[48px] flex items-center justify-center">
+                <img src={location} alt="Icono de tienda" className="w-[13px] h-[18px]" />
+              </div>
             </div>
-            {/* <MdLocationOn className='text-[#3483FA] text-3xl border rounded-full border-none bg-white p-[0.3rem] hidden sm:block' /> */}
             
             <div className='flex flex-col'>
               <p className='font-normal text-[15px]'>Retirá en Punto Pickit - Supermercado</p>
-              <p className='font-ligth text-[13px]'>Av. San Martín Sur 2575 | , Godoy Cruz - Mendoza.</p>
-              <p className='font-ligth text-[13px]'>Lunes a domingo de 9 a 19 hs.</p>
+              <p className="font-ligth text-[13px] text-[#737373]">
+                Av. San Martín Sur 2575 | , Godoy Cruz - Mendoza.
+              </p>
+              <p className="font-ligth text-[13px] text-[#737373]">Lunes a domingo de 9 a 19 hs.</p>
             </div>
 
-            <div className='flex'>
-              <span className='text-ligthblue text-xs cursor-pointer'>Modificar punto de retiro</span>
-            </div>
+            <span className="text-ligthblue text-[13px] cursor-pointer ml-auto">
+              Modificar punto de retiro
+            </span>
           </CardItem>
 
-          <CardItem>
+          <CardItem margin="mt-3" bg="bg-[#F5F5F5]">
             <div>
-              <img src={store} alt='Icono de tienda' className='border rounded-full border-none bg-black p-[1rem] w-full h-[full]' />
+              <div className="border rounded-full border-none bg-white w-[48px] h-[48px] flex items-center justify-center">
+                <img src={store} alt="Icono de tienda" className="w-[24px] h-[14px]" />
+              </div>
             </div>
             
-            <p className='font-normal text-[15px]'>Llega al punto de retiro entre el 16 y 19 de mayo</p>
+            <p className="font-normal text-[15px]">
+              Llega al punto de retiro entre el 16 y 19 de mayo
+            </p>
 
-            <p className='text-ligthblue text-xs cursor-pointer'>Modificar método de entrega</p>
+            <p className="text-ligthblue text-[13px] cursor-pointer ml-auto">
+              Modificar método de entrega
+            </p>
+
           </CardItem>
 
           <p className='text-lg text-[#333333] font-medium mb-3 mt-10'>Detalle del pago</p>
-          <CardItem>
+          <CardItem bg="bg-[#F5F5F5]">
             <div>
-              <img src={pago_facil} alt='Icono de tienda' className='border rounded-full border-none bg-black p-[0.7rem] w-full h-[full]' />
+              <img
+                src={pago_facil}
+                alt="Icono de tienda"
+                className="border rounded-full border-none bg-white p-[0.7rem] w-full h-[full]"
+              />
             </div>
 
             <div className='relative'>
@@ -53,7 +69,9 @@ const PayConfirmed = () => {
               <p className='text-[13px] text-[#737373]'>Solamente en sucursales abiertas.</p>
             </div>
 
-            <p className='text-ligthblue text-xs cursor-pointer'>Modificar medio de pago</p>
+            <p className="text-ligthblue text-[13px] cursor-pointer ml-auto">
+              Modificar medio de pago
+            </p>
           </CardItem>
         </div>
 
