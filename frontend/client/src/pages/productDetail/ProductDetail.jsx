d0b0585e7acbafde36ca0cc7f3f84f4ae81da2d7
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-28 22:58:25 -0300

    refactor: Implement StoreDetail and apply run format

--- frontend/client/src/pages/productDetail/ProductDetail.jsx
+++ frontend/client/src/pages/productDetail/ProductDetail.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Carousel from './components/Carousel';
-import DescriptionAside from './components/DescriptionAside';
-import DescriptionBottom from './components/DescriptionBottom';
-import QuestionSection from './components/QuestionSection';
-import BuySection from './BuySection';
-import StoreDetail from './StoreDetail';
-import { useParams } from 'react-router-dom';
-import { Product } from './components/Product';
-import banner from '../../assets/img/banner.svg';
+import React from "react";
+import Carousel from "./components/Carousel";
+import DescriptionAside from "./components/DescriptionAside";
+import DescriptionBottom from "./components/DescriptionBottom";
+import QuestionSection from "./components/QuestionSection";
+import BuySection from "./components/BuySection";
+import StoreDetail from "./components/StoreDetail";
+import { useParams } from "react-router-dom";
+import { Product } from "./components/Product";
+import banner from "../../assets/img/banner.svg";
 import PaidSection from "./components/PaidSection";
 
 const ProductDetail = () => {
@@ -15,28 +15,28 @@ const ProductDetail = () => {
 
   return (
     <div>
-      <div className='bg-grey flex flex-col justify-center content-center px-24 py-14 w-100'>
-        <div className='flex justify-between w-100 my-2'>
+      <div className="bg-grey flex flex-col justify-center content-center px-24 py-14 w-100">
+        <div className="flex justify-between w-100 my-2">
           <div>
             <button>Volver</button>
-            <span className='mx-1'>|</span>
-            <button className='text-ligthblue'>Celulares y teléfonos</button>
-            <span className='mx-1 text-ligthblue'>-</span>
-            <button className='text-ligthblue'>Celulares y smartphones</button>
+            <span className="mx-1">|</span>
+            <button className="text-ligthblue">Celulares y teléfonos</button>
+            <span className="mx-1 text-ligthblue">-</span>
+            <button className="text-ligthblue">Celulares y smartphones</button>
           </div>
           <div>
-            <button className='text-ligthblue'>Compartir</button>
-            <span className='text-ligthblue mx-1'>|</span>
-            <button className='text-ligthblue'>Vender uno igual</button>
+            <button className="text-ligthblue">Compartir</button>
+            <span className="text-ligthblue mx-1">|</span>
+            <button className="text-ligthblue">Vender uno igual</button>
           </div>
         </div>
-        <div className='flex'>
-          <div className='bg-white flex flex-col w-100 py-14'>
-            <div className='flex w-100 mx-16'>
-              <div className='bg-white w-3/6'>
+        <div className="flex">
+          <div className="bg-white flex flex-col w-100 py-14">
+            <div className="flex w-100 mx-16">
+              <div className="bg-white w-3/6">
                 <Carousel images={Product.images} />
               </div>
-              <div className='bg-white w-3/6'>
+              <div className="bg-white w-3/6">
                 <DescriptionAside
                   name={Product.name}
                   characteristics={Product.characteristics}
@@ -46,28 +46,28 @@ const ProductDetail = () => {
                 />
               </div>
             </div>
-            <hr className='flex mx-16 my-8' />
-            <div className='w-100 mx-16'>
+            <hr className="flex mx-16 my-8" />
+            <div className="w-100 mx-16">
               <DescriptionBottom description={Product.description} />
             </div>
-            <hr className='flex mx-16 my-8' />
-            <div className='mx-16'>
+            <hr className="flex mx-16 my-8" />
+            <div className="mx-16">
               <QuestionSection />
             </div>
           </div>
-          <div className='bg-white w-4/12 flex flex-col pt-6 pr-6 gap-4'>
+          <div className="bg-white w-4/12 flex flex-col pt-6 pr-6 gap-4">
             <BuySection />
             <StoreDetail />
             <PaidSection/>
           </div>
         </div>
         <div>Sugerencias</div>
-        <div className='flex justify-center w-100 my-14'>
-          <img src={banner} alt='banner' className='w-100' />
+        <div className="flex justify-center w-100 my-14">
+          <img src={banner} alt="banner" className="w-100" />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default ProductDetail
---
+++ frontend/client/src/pages/productDetail/StoreDetail.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BsChatSquare } from "react-icons/bs";
+import { TfiAlarmClock } from "react-icons/tfi";
+import { AiFillCheckCircle } from "react-icons/ai";
+import LogoTienda from "../../assets/img/mercadolibrelogo_solo.svg";
+function StoreDetail() {
+  return (
+    <div className="border-solid border w-[25rem] p-4 rounded-xl">
+      <h2 className="font-medium text-lg">Información de la tienda</h2>
+      <div className="flex gap-3">
+        <img src={LogoTienda} alt={"Logo"} className="w-12 h-12"></img>
+        <div>
+          <p>Mercado Libre Electrónica</p>
+          <p className="text-[#9aa0a6] text-[13px]">Tienda oficial de Mercado Libre</p>
+        </div>
+      </div>
+      <div className="inline-block my-4">
+        <ul className="inline-flex items-center gap-2 ">
+          <li className="bg-[#FFF0F0] w-12 h-2"></li>
+          <li className="bg-[#FFF5E8] w-12 h-2"></li>
+          <li className="bg-[#FFFCDA] w-12 h-2"></li>
+          <li className="bg-[#F1FDD7] w-12 h-2"></li>
+          <li className="bg-[#39B54A] w-14 h-4"></li>
+        </ul>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex flex-col items-center justify-start text-start">
+          <p className="text-[22px]">+ 100mil</p>
+          <p className="flex text-center text-[11px] mt-1">Ventas en los ultimos 60 dias</p>
+        </div>
+        <div className="flex flex-col items-center justify-start">
+          <div className="relative">
+            <BsChatSquare size={22} className="mt-2" />
+            <AiFillCheckCircle size={15} color={"#39B54A"} className="absolute left-3 top-6" />
+          </div>
+          <p className="flex text-center text-[11px] mt-2">Brinda buena atención</p>
+        </div>
+        <div className="flex flex-col items-center justify-start">
+          <div className="relative">
+            <TfiAlarmClock size={25} className="mt-2" />
+            <AiFillCheckCircle size={15} color={"#39B54A"} className="absolute left-3 top-7" />
+          </div>
+          <p className="flex text-center text-[11px] mt-2">Despacha sus productos a tiempo</p>
+        </div>
+      </div>
+      <p className="text-blue-500 mt-3 text-[13px]">Ver mas datos de este vendedor</p>
+    </div>
+  )
+}
+
+export default StoreDetail
