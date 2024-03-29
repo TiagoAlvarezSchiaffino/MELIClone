d53449e10d9e836e4c01ca554c59782fd06d5bf4
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-28 23:59:31 -0300

    feat: Add LoginNavbar

---
+++ frontend/client/src/components/Login/LoginNavbar.jsx
@@ -0,0 +1,20 @@
+import logo from "../../assets/img/logo-payment.png";
+
+const LoginNavbar = () => {
+
+    return (
+        <>
+            <div className=" bg-secondary h-[103px] sm:h-[56px] flex items-center">
+                <nav className="flex w-full mx-6 sm:mx-24">
+                    <div className="flex items-center justify-between w-full">
+                        <div>
+                            <img src={logo} alt="Mercado Libre logo" />
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default LoginNavbar
