801d9444d83674efc1bacf56b5b43d6423bb4e6d
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-03 12:31:53 -0300

    feat: Add SQL scripts to insert initial data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,10 +8,45 @@ INSERT INTO provinces (PROVINCE_ID, PROVINCE_NAME) values
   (94, "Tierra del Fuego, Antartida e Islas del Atlantico Sur" ), (90, "Tucuman" );
 
   INSERT INTO users (EMAIL, FIRST_NAME, LAST_NAME, NUMBER_PHONE, USER_PASSWORD, ROLE) VALUES
-  ('maria@correo.com','maria','test','23423', '$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'ADMIN'), 
-  ('pedro@correo.com', 'pedro','test','23423','$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'USER'),
-   ('admin@movear.com', 'admin','test','23423','$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'USER'),
-    ('user@test.com', 'user','test','23423','$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'ADMIN');
+  ('maria@meli.com','maria','test','23423', '$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'ADMIN'),
+  ('pedro@meli.com', 'pedro','test','23423','$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'USER'),
+   ('admin@meli.com', 'admin','test','23423','$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'USER'),
+    ('user@meli.com', 'user','test','23423','$2a$10$6BOKBHct.1.6NvCb2UDBuOM.w1S7SC29ggEzhYO.LlffwfxD9kjta', 'ADMIN');
+
+
+INSERT INTO category (id, name) VALUES
+(1, 'Accesorios para Vehículos'),
+(2, 'Agro'),
+(3, 'Alimentos y Bebidas'),
+(4, 'Animales y Mascotas'),
+(5, 'Antigüedades y Colecciones'),
+(6, 'Arte, Librería y Mercería'),
+(7, 'Autos, Motos y Otros'),
+(8, 'Bebés'),
+(9, 'Belleza y Cuidado Personal'),
+(10, 'Cámaras y Accesorios'),
+(11, 'Celulares y Teléfonos'),
+(12, 'Computación'),
+(13, 'Consolas y Videojuegos'),
+(14, 'Construcción'),
+(15, 'Deportes y Fitness'),
+(16, 'Electrodomésticos y Aires Ac.'),
+(17, 'Electrónica, Audio y Video'),
+(18, 'Entradas para Eventos'),
+(19, 'Herramientas'),
+(20, 'Hogar, Muebles y Jardín'),
+(21, 'Industrias y Oficinas'),
+(22, 'Inmuebles'),
+(23, 'Instrumentos Musicales'),
+(24, 'Joyas y Relojes'),
+(25, 'Juegos y Juguetes'),
+(26, 'Libros, Revistas y Comics'),
+(27, 'Música, Películas y Series'),
+(28, 'Ropa y Accesorios'),
+(29, 'Salud y Equipamiento Médico'),
+(30, 'Servicios'),
+(31, 'Souvenirs, Cotillón y Fiestas'),
+(32, 'Otras categorías');
 
 
 
