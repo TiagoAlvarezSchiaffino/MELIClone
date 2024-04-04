1e8c9693b800ec43bc404f6da16c98ae7a2fcd5d
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-04 17:08:24 -0300

    fix: Change Paypal properties

--- backend/src/main/java/com/example/demo/config/PaypalConfig.java
+++ backend/src/main/java/com/example/demo/config/PaypalConfig.java
@@ -1,7 +1,10 @@
 package com.example.demo.config;
+
+import com.example.demo.util.properties.PaypalProperties;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,29 +13,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class PaypalConfig {
 
-    @Value("${paypal.client.app}")
-    private String clientId;
-    @Value("${paypal.client.secret}")
-    private String clientSecret;
-    @Value("${paypal.mode}")
-    private String mode;
+    private final PaypalProperties paypalProperties;
 
     @Bean
-    public Map<String, String> paypalSdkConfig(){
+    public Map<String, String> paypalSdkConfig() {
         Map<String, String> sdkConfig = new HashMap<>();
-        sdkConfig.put("mode", mode);
+        sdkConfig.put("mode", paypalProperties.getMode());
         return sdkConfig;
     }
 
     @Bean
     public OAuthTokenCredential authTokenCredential() {
-        return new OAuthTokenCredential(clientId,clientSecret,paypalSdkConfig());
+        return new OAuthTokenCredential(this.paypalProperties.getClient(), this.paypalProperties.getSecret(), paypalSdkConfig());
     }
 
     @Bean
-    public APIContext apiContext() throws PayPalRESTException{
+    public APIContext apiContext() throws PayPalRESTException {
         APIContext apiContext = new APIContext(authTokenCredential().getAccessToken());
         apiContext.setConfigurationMap(paypalSdkConfig());
         return apiContext;
