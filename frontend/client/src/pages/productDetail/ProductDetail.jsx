c636475fe71ebc324f813a3e4d45d500d4724db8
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-08 10:59:04 -0300

    feat: Add product from DB by id in ProductDetail

--- frontend/client/src/pages/productDetail/ProductDetail.jsx
+++ frontend/client/src/pages/productDetail/ProductDetail.jsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Carousel from "./components/Carousel";
 import DescriptionAside from "./components/DescriptionAside";
 import DescriptionBottom from "./components/DescriptionBottom";
 import QuestionSection from "./components/QuestionSection";
 import BuySection from "./components/BuySection";
 import StoreDetail from "./components/StoreDetail";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Product } from "./components/Product";
 import banner from "../../assets/img/banner.svg";
 import PaidSection from "./components/PaidSection";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetail } from "../../store/state/productSlice";
+
 
 const ProductDetail = () => {
   const { id } = useParams();
-
+  const { detail: product } = useSelector(store => store.product);
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(getProductDetail(id))
+    window.scrollTo(0,0)
+  },[id])
   return (
     <div>
       <div className="bg-grey flex flex-col justify-center content-center px-24 py-14 w-100">
         <div className="flex justify-between w-100 my-2">
           <div>
-            <button>Volver</button>
+            <Link to="/">Volver</Link>
             <span className="mx-1">|</span>
-            <button className="text-ligthblue">Celulares y teléfonos</button>
+            <button className="text-ligthblue">{product.category?.name}</button>
             <span className="mx-1 text-ligthblue">-</span>
-            <button className="text-ligthblue">Celulares y smartphones</button>
+            <button className="text-ligthblue">{product.subcategory?.name}</button>
           </div>
           <div>
             <button className="text-ligthblue">Compartir</button>
@@ -34,21 +42,22 @@ const ProductDetail = () => {
           <div className="bg-white flex flex-col w-100 py-14">
             <div className="flex w-100 mx-16">
               <div className="bg-white w-3/6">
-                <Carousel images={Product.images} />
+                <Carousel images={product.images} />
               </div>
               <div className="bg-white w-3/6">
                 <DescriptionAside
-                  name={Product.name}
-                  characteristics={Product.characteristics}
-                  specification={Product.specification}
-                  price={Product.price}
-                  off={Product.off}
+                  name={product.title}
+                  characteristics={product.descriptionGeneric}
+                  colors={product.colors||[]}
+                  price={product.price}
+                  priceDiscount={product.priceDiscount.toFixed(2)||""}
+                  off={product.discount||""}
                 />
               </div>
             </div>
             <hr className="flex mx-16 my-8" />
             <div className="w-100 mx-16">
-              <DescriptionBottom description={Product.description} />
+              <DescriptionBottom description={product.descriptionRelevant} />
             </div>
             <hr className="flex mx-16 my-8" />
             <div className="mx-16">
@@ -58,10 +67,16 @@ const ProductDetail = () => {
           <div className="bg-white w-4/12 flex flex-col pt-6 pr-6 gap-4">
             <BuySection />
             <StoreDetail />
-            <PaidSection/>
+            <PaidSection />
           </div>
         </div>
-        <div>Sugerencias</div>
+        <div className="flex gap-4 w-100 h-100 justify-evenly py-14">
+          {/* <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard /> */}
+        </div>
         <div className="flex justify-center w-100 my-14">
           <img src={banner} alt="banner" className="w-100" />
         </div>
