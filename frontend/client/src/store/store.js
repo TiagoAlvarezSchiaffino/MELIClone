8d2314b7c0e690fa60af97c5cd4a9aa843b05cde
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-09 07:20:23 -0300

    feat: Configure Redux store with slices

--- frontend/client/src/store/store.js
+++ frontend/client/src/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./state/authSlice";
+import categories from "./state/categoriesSlice";
+import product from "./state/productSlice";
+import cart from "./state/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    auth
+    auth,
+    categories,
+    product,
+    cart
   }
 })
