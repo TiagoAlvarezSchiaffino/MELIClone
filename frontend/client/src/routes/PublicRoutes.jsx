e50f66fad84174ca0f3d7ce7eeb16ea0febb2987
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-09 07:42:47 -0300

    feat: Add new routes and authentication

--- frontend/client/src/routes/PublicRoutes.jsx
+++ frontend/client/src/routes/PublicRoutes.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import ProductDetail from "../pages/productDetail/ProductDetail";
-import Navbar from "../components/NavBar/NavBar";
+import Navbar from "../components/Navbar/Navbar";
+import ProductsList from "../pages/ProductsList/ProductsList";
+import AllCategories from "../pages/AllCategories";
 
 const PublicRoutes = () => {
   return (
@@ -11,6 +13,9 @@ const PublicRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product-detail/:id" element={<ProductDetail />} />
+        <Route path="/product-list/:title" element={<ProductsList />} />
+        <Route path="/product-list/category/:idCategory/:nameCategory" element={<ProductsList />} />
+        <Route path="all-categories" element={<AllCategories />} />
       </Routes>
     </>
   )
