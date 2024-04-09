ef8932cf01364d923c1ef48a407f31718b480c8f
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-04-09 09:24:39 -0300

    feat: Load data

--- backend/src/main/java/com/example/demo/model/entity/Product.java
+++ backend/src/main/java/com/example/demo/model/entity/Product.java
@@ -4,49 +4,67 @@ package com.example.demo.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Entity
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private Integer id;
-    private String name;
-    private double price;
+    private String title;
+    private Double price;
     private Integer stock;
+    private Integer discount;
+    private Double priceDiscount;
+    private Integer numberQuotas;
 
-    @Column(name = "DESCRIPTION", columnDefinition="TEXT", nullable = false)
-    @Lob
-    private String description;
+    @Transient
+    private Double priceQuotas;
 
-    @ManyToOne
-    @JoinColumn(name = "Category_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
     private Category category;
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(fetch = FetchType.LAZY,
+            mappedBy = "product",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
     private List<Image> images;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "SubCategory_id")
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    @Builder.Default
+    private Set<OrderItem> items = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subcategory_id")
     private Subcategory subcategory;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "PRODUCT_ID")
-    @Builder.Default
-    private Set<OrderItem> items=new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
+    List<Description> descriptions;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "colors_has_products", joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "color_id"))
+    private List<Color> productJoined;
 
 }
